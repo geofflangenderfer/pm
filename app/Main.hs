28f69dfd486198ad0de216dcfb7215a9f2834c57
geoff langenderfer <geoff.langenderfer@gmail.com>
2023-04-07 11:57:40 -0700

    rename effortEstimate -> hoursEstimate

--- app/Main.hs
+++ app/Main.hs
@@ -17,24 +17,23 @@ data Task = Task
   { description :: String
   , impact :: Int
   , urgency :: Int
-  , effortEstimate :: Int
+  , hoursEstimate :: Int
   , roi :: Double
   } deriving (Show, Generic, ToJSON, FromJSON)
 
 
 instance Eq Task where
     t1 == t2 =
-        (description t1, impact t1, urgency t1, effortEstimate t1, roi t1)
-        == (description t2, impact t2, urgency t2, effortEstimate t2, roi t2)
+        (description t1, impact t1, urgency t1, hoursEstimate t1, roi t1)
+        == (description t2, impact t2, urgency t2, hoursEstimate t2, roi t2)
 
 instance Ord Task where
   t1 `compare` t2 =
-    let priority1 = (impact t1 * urgency t1) `div` effortEstimate t1
-        priority2 = (impact t2 * urgency t2) `div` effortEstimate t2
+    let priority1 = (impact t1 * urgency t1) `div` hoursEstimate t1
+        priority2 = (impact t2 * urgency t2) `div` hoursEstimate t2
     in priority2 `compare` priority1
 
 type TaskQueue = Heap.Heap Task
-
 -- Prompt the user for a string input
 promptString :: String -> IO String
 promptString prompt = do
@@ -60,9 +59,9 @@ promptTask = do
   description <- promptString "Enter a task description: "
   impact <- promptInt "Enter the impact of the task (1-10): "
   urgency <- promptInt "Enter the urgency of the task (1-10): "
-  effortEstimate <- promptInt "Enter the effort estimate of the task (in hours): "
+  hoursEstimate <- promptInt "Enter the effort estimate of the task (in hours): "
   roi <- promptDouble "Enter the expected ROI of the task (as a decimal): "
-  return (Task description impact urgency effortEstimate roi)
+  return (Task description impact urgency hoursEstimate roi)
 
 
 writeQueueToFile :: TaskQueue -> IO ()
